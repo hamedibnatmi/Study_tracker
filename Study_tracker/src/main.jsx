85bbbffd08a59c61a2f5231f040db0a63c4338fe
hamedibnatmi <hamed2599@gmail.com>
2026-02-18 15:08:32 +0200

    Added Mantine library

--- Study_tracker/src/main.jsx
+++ Study_tracker/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
