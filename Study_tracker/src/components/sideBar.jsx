0b601885b2bc0260ccec1c514412a1b5e693444b
hamedibnatmi <hamed2599@gmail.com>
2026-02-19 09:08:40 +0200

    Handle SideBar Elements and CSS

--- Study_tracker/src/components/sideBar.jsx
+++ Study_tracker/src/components/sideBar.jsx
@@ -2,8 +2,25 @@
 
 const SideBar = () => {
     return (
-        <div>
-            side Bar
+        <div className="side-bar">
+            <div className="side-bar-header">
+                <h2>Study Tracker</h2>
+            </div>
+            <div className="side-bar-content">
+                <ul>
+                    <li>
+                        <a href="#">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#">History</a>
+                    </li>
+                    <li>
+                        <a href="#">Profile</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
+
+export default SideBar
