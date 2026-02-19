0059cb072e32debed7452974d2ffd919c11e116a
hamedibnatmi <hamed2599@gmail.com>
2026-02-19 12:02:21 +0200

    Add icons to SideBar

--- Study_tracker/src/components/sideBar.jsx
+++ Study_tracker/src/components/sideBar.jsx
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router-dom"
+import { House, History, User, BookOpen } from "lucide-react"
 
 const SideBar = () => {
     return (
         <div className="side-bar">
             <div className="side-bar-header">
-                <h2>Study Tracker</h2>
+                <h2><BookOpen size={30} color="purple" />Study Tracker</h2>
             </div>
             <div className="side-bar-content">
                 <nav className="side-bar-nav">
-                    <NavLink className="NavLink" to="/">Dashboard</NavLink>
-                    <NavLink className="NavLink" to="/history">History</NavLink>
-                    <NavLink className="NavLink" to="/profile">Profile</NavLink>
+                    <NavLink className="NavLink" to="/"><House />Dashboard</NavLink>
+                    <NavLink className="NavLink" to="/history"><History />History</NavLink>
+                    <NavLink className="NavLink" to="/profile"><User />Profile</NavLink>
                 </nav>
             </div>
         </div>
