73cb26125e396d4786dd49e3558f077cdfdb9ea8
hamedibnatmi <hamed2599@gmail.com>
2026-02-19 08:02:20 +0200

    Added Example how to fetch data form Supabase

--- Study_tracker/src/App.jsx
+++ Study_tracker/src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import supabase from './SupaBase'
 
 function App() {
-  console.log("sup", supabase)
+
+  // This example how to fetch data from Supabase
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = await supabase.from("StudyTracker").select("*").eq("courses__userId", "user-001")
+      if (error) {
+        console.error("Error fetching courses:", error)
+      } else {
+        console.log("Fetched courses:", data)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
       <div>
