1ac4d6f88de19dc7d97cea0c390db562f0fc60b6
hamedibnatmi <hamed2599@gmail.com>
2026-02-22 12:38:27 +0200

    Add Add subtask button

--- Study_tracker/src/components/subTasks.jsx
+++ Study_tracker/src/components/subTasks.jsx
@@ -1,5 +1,6 @@
 import { useAppContext } from "../context/AppContext"
-
+import { Plus } from "lucide-react"
+import { useState } from "react"
 const SubTasks = () => {
     const { courses, completedTasks, getAllSubTasks, checkSubTask, refetch, setRefetch, user } = useAppContext()
     const handleCheck = async (subtaskId, completed) => {
@@ -11,12 +12,27 @@ const SubTasks = () => {
             setRefetch(!refetch)
         }
     }
+    const [isCoursePage, setIsCoursePage] = useState(window.location.pathname.includes("/course"))
+    const [showAddSubTaskForm, setShowAddSubTaskForm] = useState(false)
     return (<>
         <div className="subtasks">
             <div className="subtasks-title">
                 <h2>Tasks Due Today</h2>
-                <p>{completedTasks}/{getAllSubTasks(courses).length} done</p>
+                <div className="right-side">
+                    {!isCoursePage && <p>{completedTasks}/{getAllSubTasks(courses).length} done</p>}
+                    {isCoursePage && <span className="add-subtask" onClick={() => setShowAddSubTaskForm(!showAddSubTaskForm)}><Plus />Add Task</span>}
+                </div>
             </div>
+            {showAddSubTaskForm && <div className="add-task-form">
+                <form action="">
+                    <label htmlFor="task-title">Task Title</label>
+                    <input type="text" id="task-title" placeholder="Enter Task Title" />
+                    <label htmlFor="task-due-date">Task Due Date</label>
+                    <input type="date" id="task-due-date" placeholder="Enter Task Due Date" />
+                    <button type="submit">Add Task</button>
+                </form>
+            </div>
+            }
             {courses.map((course) => (
                 course.subtasks.map((subtask) => (
 
