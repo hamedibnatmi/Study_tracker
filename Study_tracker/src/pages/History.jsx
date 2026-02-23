a28ead4cfce12ce2de1a5deb65faa37320013685
hamedibnatmi <hamed2599@gmail.com>
2026-02-23 03:00:02 +0200

    Handle Table history in  History Page

--- Study_tracker/src/pages/History.jsx
+++ Study_tracker/src/pages/History.jsx
@@ -1,12 +1,24 @@
-import { Trash2 } from "lucide-react"
+import { Trash2, Calendar } from "lucide-react"
+import { useAppContext } from "../context/AppContext"
 const History = () => {
+    const { totalHoursUntilNow, totalSessions, completedTasks, studySessions, courses, getCourseDuration } = useAppContext()
+    console.log("courses", courses)
+    const calculteTime = (time) => {
+        const hours = Math.floor(time / 60)
+        const minutes = time % 60
+        return `${hours}h ${minutes}m`
+    }
     return (
-        <div>
-            <h1 className="history-page-title">Study History</h1>
+        <div className="history-page">
+            <div className="title">
+                <h1>Study History</h1>
+                <h5>Here's your study history.</h5>
+            </div>
             <div className="history-page-container">
                 <div className="total-time-box">
                     <p>Total Study Time</p>
-                    <p>0</p>
+                    <p className="total-time">{calculteTime(totalHoursUntilNow)}</p>
+                    <p className="total-sessions">{studySessions.length} sessions recorded</p>
                 </div>
                 <div className="history-list">
                     <p>History</p>
@@ -20,12 +32,14 @@ const History = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Math</td>
-                                <td>1 hour</td>
-                                <td>2022-01-01</td>
-                                <td><button className="delete-action"><Trash2 /></button></td>
-                            </tr>
+                            {courses.map((course) => (
+                                <tr key={course.id}>
+                                    <td>{course.title}</td>
+                                    <td>{getCourseDuration(course.study_sessions)}</td>
+                                    <td><Calendar size={15} style={{ marginRight: "5px" }} />{course.created_at.slice(0, 10)}</td>
+                                    <td><button className="delete-action"><Trash2 /></button></td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
